731266053547052798b8025c3f4e26f84600cd74
criverao <c.riverao@uniandes.edu.co>
2023-03-14 19:18:57 -0500

    Agregar Datepicker a la creación de alarmas

--- app/src/main/java/com/example/alarmapp/ui/alarms/CreateAlarmFragment.kt
+++ app/src/main/java/com/example/alarmapp/ui/alarms/CreateAlarmFragment.kt
@@ -1,15 +1,19 @@
 package com.example.alarmapp.ui.alarms
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.example.alarmapp.R
 import com.example.alarmapp.databinding.FragmentCreateAlarmBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CreateAlarmFragment : Fragment() {
 
@@ -25,6 +29,35 @@ class CreateAlarmFragment : Fragment() {
 
         _binding = FragmentCreateAlarmBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val textviewDate: TextView?
+        val cal = Calendar.getInstance()
+
+        textviewDate = binding.textViewDate1
+
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                val myFormat = "MM/dd/yyyy" // mention the format you need
+                val sdf = SimpleDateFormat(myFormat, Locale.US)
+                textviewDate.text = sdf.format(cal.time)
+            }
+
+        // when you click on the button, show DatePickerDialog that is set with OnDateSetListener
+        textviewDate.setOnClickListener {
+            context?.let { it1 ->
+                DatePickerDialog(
+                    it1,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
 
         ArrayAdapter.createFromResource(
             requireContext(),
